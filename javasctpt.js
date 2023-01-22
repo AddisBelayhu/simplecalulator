9576546531a6ca68f46ca60d5c3d627203e6a7fd
Addis Belayhun <107222638+AddisBelayhu@users.noreply.github.com>
2023-01-22 14:11:13 +0300

    Add files via upload

---
+++ javasctpt.js
@@ -0,0 +1,23 @@
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
+ function moduloformula(){
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 % num2;
+ }
+
+ function clearValue(){
+    num1 = document.getElementById("firstNumber").value = '';
+    num2 = document.getElementById("secondNumber").value = ''; 
+     }
